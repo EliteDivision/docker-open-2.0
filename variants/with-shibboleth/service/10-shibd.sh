b81ecf2e5cbe7762fbb0e5e552d10a67921b757c
Damian Gomez <racksoft@gmail.com>
2024-12-13 10:12:34 +0100

    Env Vars for SP backend and frontend

--- variants/with-shibboleth/service/10-shibd.sh
+++ variants/with-shibboleth/service/10-shibd.sh
@@ -1,6 +1,11 @@
 if [ "$ENABLE_SHIBD" = "false" ] ; then
     rm /etc/supervisor/conf.d/shibd.conf
 else
+    # Setup AppID
+    sed -i "s/SP_FRONTEND_ID/${SP_FRONTEND_ID}/" /etc/apache2/sites-available/000-default.conf
+    sed -i "s/SP_BACKEND_ID/${SP_BACKEND_ID}/" /etc/apache2/sites-available/000-default.conf
+
+    #Reset Service (thanks debian...)
     service shibd restart
     service shibd stop
 fi
