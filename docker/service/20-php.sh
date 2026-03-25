c74ab4274e87dcd583a3fc78c356af8284c44dd0
Damian Gomez <racksoft@gmail.com>
2026-03-25 21:03:04 +0100

    Pool limits experiment

--- docker/service/20-php.sh
+++ docker/service/20-php.sh
@@ -3,4 +3,5 @@ if [ "$ENABLE_PHP" = "false" ] ; then
 else
     sed -i "s/PHP_VERSION/${PHP_VERSION}/" /etc/supervisor/conf.d/php.conf
     sed -i "s/PHP_VERSION/${PHP_VERSION}/" /etc/apache2/conf-enabled/95-php.conf
+    sed -i "s/PHP_MAX_CHILDREN/${PHP_MAX_CHILDREN}/" /etc/php/${PHP_VERSION}/fpm/pool.d/www.conf
 fi
